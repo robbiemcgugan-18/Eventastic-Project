e16f1bc857cc828cade14c8a0b3499fb27d04b13
Robbie McGugan <2523558M@student.gla.ac.uk>
2021-03-10 20:01:49 +0000

    updated base.html and index.html and added DB tables

--- team_10e_app/admin.py
+++ team_10e_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from team_10e_app.models import Category, UserProfile, Event, Attend, Comment
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(UserProfile)
+admin.site.register(Event)
+admin.site.register(Attend)
+admin.site.register(Comment)
--- team_10e_app/models.py
+++ team_10e_app/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    categoryName = models.CharField(max_length=30, unique=True)
+    categoryPicture = models.ImageField()
+    categoryDescription = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.categoryName
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    DOB = models.DateField()
+    profilePicture = models.ImageField()
+
+    def __str__(self):
+        return self.user.username
+
+class Event(models.Model):
+    eventName = models.CharField(max_length=30, unique=True)
+    eventDescription = models.CharField(max_length=200)
+    start = models.DateTimeField()
+    numberInterested = models.IntegerField()
+    eventPicture = models.ImageField()
+    address = models.CharField(max_length=40)
+    postcode = models.CharField(max_length=8)
+    averageRating = models.DecimalField(max_digits=3, decimal_places=2)
+    createdBy = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.eventName
+
+class Attend(models.Model):
+    eventName = models.ForeignKey(Event, on_delete=models.CASCADE)
+    username = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    class Meta:
+        UniqueConstraint(fields=['eventName', 'username'], name='userAttends')
+
+class Comment(models.Model):
+    eventName = models.ForeignKey(Event, on_delete=models.CASCADE)
+    username = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    posted = models.DateTimeField(default=timezone.now())
+    comment = models.CharField(max_length=100)
+
+    class Meta:
+        UniqueConstraint(fields=['eventName', 'username', 'posted'], name='userComment')

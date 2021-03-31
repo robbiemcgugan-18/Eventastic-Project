181ab6f5f9af53d5bf10347021e26af1f60f08f6
Liang420436 <78217131+Liang420436@users.noreply.github.com>
2021-03-31 18:07:07 +0100

    add pictures

--- eventastic/forms.py
+++ eventastic/forms.py
@@ -11,10 +11,6 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password',)
 
-        help_texts = {
-            'username': None,
-        }
-
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
@@ -40,7 +36,7 @@ class UserProfileForm(forms.ModelForm):
         fields = ('DOB', 'profilePicture')
 
         widgets = {
-            'DOB': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'YYYY-MM-DD'}),
+            'DOB': forms.DateInput(attrs={'class': 'form-control'}),
             'profilePicture': forms.FileInput(attrs={'class': 'form-control'}),
         }
 

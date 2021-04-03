b6c7aee14cbd1247048b4eb19125b0315fdcee9d
Robbie McGugan <2523558M@student.gla.ac.uk>
2021-04-03 01:59:16 +0100

    improved population script

--- populate_data.py
+++ populate_data.py
@@ -19,20 +19,22 @@ def clear_db():
 
 def generate_data():
     # generate 3 user
-    user1 = User(username="John", email="john@example.com")
-    user2 = User(username="Mary", email="mary@example.com")
-    user3 = User(username="Kate", email="kate@example.com")
-    for user_ins in [user1, user2, user3]:
-        user_ins.set_password("maggie05")
-        user_ins.save()
-    # generate 3 user profile
-    profile1 = UserProfile(user=user1, DOB=datetime.datetime(1990, 1, 1))
-    profile2 = UserProfile(user=user2, DOB=datetime.datetime(1995, 1, 1))
-    profile3 = UserProfile(user=user3, DOB=datetime.datetime(2000, 1, 1))
+    user1 = User(username="john46", first_name="John", last_name="Doe", email="john@example.com")
+    user2 = User(username="mary95", first_name="Mary", last_name="Smith", email="mary@example.com")
+    user3 = User(username="kate25", first_name="Kate", last_name="McDonald", email="kate@example.com")
 
-    profile1.profilePicture = ImageFile(open("default.jpg", 'rb'))
-    profile2.profilePicture = ImageFile(open("default.jpg", 'rb'))
-    profile3.profilePicture = ImageFile(open("default.jpg", 'rb'))
+    for user_ins in [user1, user2, user3]:
+        user_ins.set_password("default123")
+        user_ins.save()
+
+    # generate 3 user profile
+    profile1 = UserProfile(user=user1, DOB=datetime.datetime(1990, 5, 18))
+    profile2 = UserProfile(user=user2, DOB=datetime.datetime(1995, 12, 1))
+    profile3 = UserProfile(user=user3, DOB=datetime.datetime(2000, 1, 31))
+
+    profile1.profilePicture = ImageFile(open("population_images/profile_images/user1.jpg", 'rb'))
+    profile2.profilePicture = ImageFile(open("population_images/profile_images/user2.jpg", 'rb'))
+    profile3.profilePicture = ImageFile(open("population_images/profile_images/user3.jpg", 'rb'))
 
     for profile_ins in [profile1, profile2, profile3]:
         profile_ins.save()
@@ -42,9 +44,9 @@ def generate_data():
     cooking_category = Category(name="Cooking", description="Here is Cooking description")
     sport_category = Category(name="Sport", description="Here is Bake Sales description")
 
-    aviation_category.picture = ImageFile(open("default.jpg", 'rb'))
-    cooking_category.picture = ImageFile(open("default.jpg", 'rb'))
-    sport_category.picture = ImageFile(open("default.jpg", 'rb'))
+    aviation_category.picture = ImageFile(open("population_images/category_images/aviation.jpg", 'rb'))
+    cooking_category.picture = ImageFile(open("population_images/category_images/cooking.jpg", 'rb'))
+    sport_category.picture = ImageFile(open("population_images/category_images/sport.jpg", 'rb'))
 
     aviation_category.save()
     cooking_category.save()
@@ -68,7 +70,7 @@ def generate_data():
     cooking_event_2.picture = ImageFile(open("population_images/event_images/cooking2.jpg", 'rb'))
     cooking_event_3.picture = ImageFile(open("population_images/event_images/cooking3.jpg", 'rb'))
 
-    sport_event_1 = Event(name="Football Meet up", description="Quick meet up to play a game of football", startDate=datetime.datetime(2020, 1, 1), startTime=datetime.time(15, 00), address="941, Pollokshaws Road, Glasgow",postcode="G41 2EU", createdBy=profile1, category=sport_category)
+    sport_event_1 = Event(name="Football Meet up", description="Quick meet up to play a game of football", startDate=datetime.datetime(2020, 1, 1), startTime=datetime.time(15, 00), address="941, Pollokshaws Road, Glasgow",postcode="G41 2DX", createdBy=profile1, category=sport_category)
     sport_event_2 = Event(name="Tennis Tournament", description="Sign up to prove you are the best tennis player", startDate=datetime.datetime(2020, 2, 1), startTime=datetime.time(15, 00), address="130 Clyde Street, Glasgow", postcode="G1 4LH", createdBy=profile1, category=sport_category)
     sport_event_3 = Event(name="Basketball Session", description="Need a few more players to get a good game of basketball", startDate=datetime.datetime(2020, 3, 1), startTime=datetime.time(15, 00), address="196, Ayr Rd, Newton Mearns, Glasgow", postcode="G77 6DT", createdBy=profile2, category=sport_category)
 
@@ -90,25 +92,34 @@ def generate_data():
 
     # user interested m2m many to many
     cooking_event_1.usersInterested.add(profile1)
-    cooking_event_2.usersInterested.add(profile2)
+    cooking_event_1.usersInterested.add(profile2)
+    cooking_event_1.usersInterested.add(profile3)
+    aviation_event_2.usersInterested.add(profile2)
+    aviation_event_2.usersInterested.add(profile3)
+    sport_event_3.usersInterested.add(profile2)
+    sport_event_3.usersInterested.add(profile1)
+    sport_event_1.usersInterested.add(profile1)
+    cooking_event_3.usersInterested.add(profile3)
+
+    cooking_event_1.numberInterested = 3
+    aviation_event_2.numberInterested = 2
+    sport_event_3.numberInterested = 2
+    sport_event_1.numberInterested = 1
+    cooking_event_3.numberInterested = 1
 
     cooking_event_1.save()
-    cooking_event_2.save()
-    sport_event_3.save()
     aviation_event_2.save()
+    sport_event_3.save()
+    sport_event_1.save()
+    cooking_event_3.save()
 
-    cooking_event_1.numberInterested = 1
-    cooking_event_2.numberInterested = 1
-
-    cooking_event_1.save()
-    cooking_event_2.save()
     # generate comment
-    Comment(name=cooking_event_1, username=profile1, comment="event1 is so nice").save()
-    Comment(name=cooking_event_1, username=profile1, comment="event2 is just so so").save()
-    Comment(name=aviation_event_2, username=profile2, comment="I think this looks ok").save()
-    Comment(name=cooking_event_2, username=profile2, comment="I think event1 is nice").save()
-    Comment(name=aviation_event_2, username=profile3, comment="event3 is well").save()
-    Comment(name=sport_event_3, username=profile2, comment="event3 is good").save()
+    Comment(name=cooking_event_1, username=profile1, comment="I can't wait, this looks very fun").save()
+    Comment(name=cooking_event_1, username=profile2, comment="This one isn't for me").save()
+    Comment(name=aviation_event_2, username=profile2, comment="This looks OK, the location is quite hard to get to.").save()
+    Comment(name=cooking_event_2, username=profile2, comment="I am so excited for this one, it will make my week").save()
+    Comment(name=aviation_event_2, username=profile3, comment="I've loved this all my life, so glad I've found people who also share the same interest").save()
+    Comment(name=sport_event_3, username=profile2, comment="Can't wait for a little workout").save()
 
 
 if __name__ == "__main__":
